0bcadba7988d0e04b3f8888034ac1be7ea149595
Franco Meloni <franco.meloni91@gmail.com>
2019-02-16 19:17:39 +0000

    Fix Dangerfiles

--- CustomPath.swift
+++ CustomPath.swift
@@ -1,4 +1,6 @@
 import Danger
+import DangerSwiftCoverage
+
 let danger = Danger()
 
-Coverage.xcodeBuildCoverage(.custom("build/result"), minimumCoverage: 50, excludedTargets: [])
+Coverage.xcodeBuildCoverage(.custom("build/result"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
--- Dangerfile.swift
+++ Dangerfile.swift
@@ -7,4 +7,4 @@ let danger = Danger()
 let report = XCodeSummary(filePath: "result.json")
 report.report()
 
-Coverage.xcodeBuildCoverage(.derivedDataFolder("build"), minimumCoverage: 50, excludedTargets: [])
+Coverage.xcodeBuildCoverage(.derivedDataFolder("build"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
