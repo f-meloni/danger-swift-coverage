18ed77e3d4c69e9ff014a3e457b106013e0ae6d7
Franco Meloni <franco.meloni91@gmail.com>
2020-01-12 15:06:15 +0000

    Change result name

--- CustomPath.swift
+++ CustomPath.swift
@@ -3,4 +3,4 @@ import DangerSwiftCoverage
 
 let danger = Danger()
 
-Coverage.xcodeBuildCoverage(.xcresultBundle("build/results.xcresult"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
+Coverage.xcodeBuildCoverage(.xcresultBundle("build/result.xcresult"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
