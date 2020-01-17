bb61ce4174ff1afa82c1e131e43d19aabb87c918
Franco Meloni <franco.meloni91@gmail.com>
2020-01-17 20:27:51 +0000

    Re add DangerXcodeSummary

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,6 +1,10 @@
 import Danger
 import DangerSwiftCoverage
+import DangerXCodeSummary
 
 let danger = Danger()
+
+let report = XCodeSummary(filePath: "result.json")
+report.report()
 
 Coverage.xcodeBuildCoverage(.derivedDataFolder("build"), minimumCoverage: 50, excludedTargets: ["DangerSwiftCoverageTests.xctest"])
